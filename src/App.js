6e04e0f8d6d8d4c46c853747c8481d1f0e26ced6
Kumar Krishnamoorthy <kumar.softindies@gmail.com>
2021-08-15 12:06:36 +0530

    Sample1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {MyCalc} from './MyCalc.js';
+
+function App() {
+  return (
+    <div className="App">
+	<MyCalc/>
+    </div>
+  );
+}
+
+export default App;
